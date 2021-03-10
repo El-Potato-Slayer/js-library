2bd9f13c2dd87b9354ba8221fb46e9480d1d67b5
Your Name <basaijapascal9@gmail.com>
2021-03-10 17:15:29 +0300

    Add remove book fuctionality to book lib.

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+
 for (let i = 0; i < myLibrary.length; i++) {
   bookList = document.getElementById('book-list')
   book = bookList.appendChild(document.createElement('div'))
   book.classList.add('card')
-  title = book.appendChild(document.createElement('h4'))
-  author = book.appendChild(document.createElement('p'))
-  pages = book.appendChild(document.createElement('p'))
-  hasRead = book.appendChild(document.createElement('p'))
-  title.innerHTML = `${myLibrary[i].title}`
-  author.innerHTML = `Author: ${myLibrary[i].author}`
-  pages.innerHTML = `Number of pages: ${myLibrary[i].pages}`
-  hasRead.innerHTML = `Has read? ${myLibrary[i].hasRead}`
+  book.appendChild(document.createElement('h4')).innerHTML = `${myLibrary[i].title}`;
+  book.appendChild(document.createElement('p')).innerHTML = `Author: ${myLibrary[i].author}`;
+  book.appendChild(document.createElement('p')).innerHTML = `Number of pages: ${myLibrary[i].pages}`;
+  book.appendChild(document.createElement('p')).innerHTML = `Has read? ${myLibrary[i].hasRead}`;
+  btn = book.appendChild(document.createElement('button'))
+  btn.innerHTML = "Delete" 
+  btn.classList.add('delete')
 }
 
 let addBook = document.getElementById("submit")
@@ -40,7 +40,20 @@ addBook.addEventListener('click', e => {
   book.classList.add('card')
   bookTitle = book.appendChild(document.createElement('h4'))
   bookTitle.innerHTML = title
-  // author = book.appendChild(document.createElement('p'))
-  // pages = book.appendChild(document.createElement('p'))
-  // hasRead = book.appendChild(document.createElement('p'))
+  bookAuthor = book.appendChild(document.createElement('p'))
+  bookAuthor.innerHTML = author
+  bookPages = book.appendChild(document.createElement('p'))
+  bookPages.innerHTML = pages
+  hasReadBook = book.appendChild(document.createElement('p'))
+  hasReadBook.innerHTML = "false"
+  
+})
+
+let removeBtns = document.querySelectorAll('.delete')
+removeBtns.forEach((btn,index) =>{
+  btn.addEventListener('click', () =>{
+    myLibrary.splice(index,1)
+    console.log(myLibrary)
+    btn.parentElement.remove()
+  })
 })
