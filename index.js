e9fb9694f76122a98f735ce5f3df2618d02404b9
Your Name <basaijapascal9@gmail.com>
2021-03-11 16:17:11 +0300

    fix javscript code according to linter requirements

--- index.js
+++ index.js
@@ -1,97 +1,102 @@
-let myLibrary = [
-  { author: "Jack", title: "Title of book", pages: 400, hasRead: false },
-  { author: "Hoppie", title: "Ma se poes", pages: 3, hasRead: false },
-  { author: "Mary", title: "A book", pages: 200, hasRead: false }
-]
-let bookList = document.getElementById('book-list')
-let addBook = document.getElementById("submit")
-let removeBtns;
-let readBtns;
+const myLibrary = [
+  {
+    author: 'Jack', title: 'Title of book', pages: 400, hasRead: false,
+  },
+  {
+    author: 'Hoppie', title: 'Ma se poes', pages: 3, hasRead: false,
+  },
+  {
+    author: 'Mary', title: 'A book', pages: 200, hasRead: false,
+  },
+];
+const bookList = document.getElementById('book-list');
+const addBook = document.getElementById('submit');
+
 
 function Book(author, title, pages, hasRead) {
-  this.author = author
-  this.title = title
-  this.pages = pages
-  this.hasRead = hasRead
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.hasRead = hasRead;
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
 
 function addClickEventForDelete(buttons) {
-  buttons.forEach((btn,index) => {
-    btn.addEventListener('click', () =>{
-      myLibrary.splice(index,1)
-      btn.parentElement.remove()
-    })
-  })
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      myLibrary.splice(index, 1);
+      btn.parentElement.remove();
+    });
+  });
 }
 
 function addClickEventForEdit(buttons) {
-  buttons.forEach((btn,index) => {
-    btn.addEventListener('click', () =>{
-      myLibrary[index].hasRead ? myLibrary[index].hasRead = false : myLibrary[index].hasRead = true
-      readElement = document.querySelectorAll('.card')[index].children[3]
-      readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`
-    })
-  })
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      myLibrary[index].hasRead = !myLibrary[index].hasRead;
+      const readElement = document.querySelectorAll('.card')[index].children[3];
+      readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`;
+    });
+  });
 }
 
-function createBookCard(bookList, book){
-  card = bookList.appendChild(document.createElement('div'))
-  card.classList.add('card')
+function createBookCard(bookList, book) {
+  const card = bookList.appendChild(document.createElement('div'));
+  card.classList.add('card');
   card.appendChild(document.createElement('h4')).innerHTML = `${book.title}`;
   card.appendChild(document.createElement('p')).innerHTML = `Author: ${book.author}`;
   card.appendChild(document.createElement('p')).innerHTML = `Pages: ${book.pages}`;
   card.appendChild(document.createElement('p')).innerHTML = `Has read? ${book.hasRead}`;
-  return card
+  return card;
 }
 
 function createButton(container, btnName) {
-  dltBtn = container.appendChild(document.createElement('button'))
-  dltBtn.innerHTML = btnName
-  dltBtn.classList.add(btnName.toLowerCase())
-  dltBtn.classList.add('btn')
+  const btn = container.appendChild(document.createElement('button'));
+  btn.innerHTML = btnName;
+  btn.classList.add(btnName.toLowerCase());
+  btn.classList.add('btn');
 }
 
-for (let i = 0; i < myLibrary.length; i++) {
-  let bookCard = createBookCard(bookList, myLibrary[i])
-  dltBtn = bookCard.appendChild(document.createElement('button'))
-  dltBtn.innerHTML = "Delete" 
-  dltBtn.classList.add('delete')
-  dltBtn.classList.add('btn')
-  readBtn = bookCard.appendChild(document.createElement('button'))
-  readBtn.innerHTML = "Read" 
-  readBtn.classList.add('read')
-  readBtn.classList.add('btn')
+for (let i = 0; i < myLibrary.length; i += 1) {
+  const bookCard = createBookCard(bookList, myLibrary[i]);
+  const dltBtn = bookCard.appendChild(document.createElement('button'));
+  dltBtn.innerHTML = 'Delete';
+  dltBtn.classList.add('delete');
+  dltBtn.classList.add('btn');
+  const readBtn = bookCard.appendChild(document.createElement('button'));
+  readBtn.innerHTML = 'Read';
+  readBtn.classList.add('read');
+  readBtn.classList.add('btn');
 }
 
-readBtns = document.querySelectorAll('.read')
+const readBtns = document.querySelectorAll('.read');
 
-addClickEventForEdit(readBtns)
+addClickEventForEdit(readBtns);
 
 addBook.addEventListener('click', () => {
-  title = document.getElementById('title').value
-  author = document.getElementById('author').value
-  pages = document.getElementById('pages').value
-  let book = new Book(title, author, pages, false)
-  addBookToLibrary(book)
-  bookCard = createBookCard(bookList, book)
-  createButton(bookCard, "Delete")
-  removeBtns = document.querySelectorAll('.delete')
-  addClickEventForDelete(removeBtns)
-  createButton(bookCard, "Read")
-  readBtns = document.querySelectorAll('.read')
-  let index = readBtns.length - 1
-  readBtns[readBtns.length - 1].addEventListener('click', () =>{
-    myLibrary[index].hasRead ? myLibrary[index].hasRead = false : myLibrary[index].hasRead = true
-    readElement = document.querySelectorAll('.card')[index].children[3]
-    readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`
-  })
-})
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const book = new Book(title, author, pages, false);
+  addBookToLibrary(book);
+  const bookCard = createBookCard(bookList, book);
+  createButton(bookCard, 'Delete');
+  const removeBtns = document.querySelectorAll('.delete');
+  addClickEventForDelete(removeBtns);
+  createButton(bookCard, 'Read');
+  const readBtns = document.querySelectorAll('.read');
+  const index = readBtns.length - 1;
+  readBtns[readBtns.length - 1].addEventListener('click', () => {
+    myLibrary[index].hasRead = !myLibrary[index].hasRead;
+    const readElement = document.querySelectorAll('.card')[index].children[3];
+    readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`;
+  });
+});
 
-removeBtns = document.querySelectorAll('.delete')
+const removeBtns = document.querySelectorAll('.delete');
 
-addClickEventForDelete(removeBtns)
 
+addClickEventForDelete(removeBtns);
