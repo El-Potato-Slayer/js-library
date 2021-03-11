17c2783b9fe7d4d78231cbb4d36916f425d642a1
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-03-11 14:06:33 +0200

    added styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let myLibrary = [
 let bookList = document.getElementById('book-list')
 let addBook = document.getElementById("submit")
 let removeBtns;
-let editBtns;
+let readBtns;
 
 function Book(author, title, pages, hasRead) {
   this.author = author
@@ -31,11 +31,7 @@ function addClickEventForDelete(buttons) {
 function addClickEventForEdit(buttons) {
   buttons.forEach((btn,index) => {
     btn.addEventListener('click', () =>{
-      if (myLibrary[index].hasRead) {
-        myLibrary[index].hasRead = false
-      }else {
-        myLibrary[index].hasRead = true
-      }
+      myLibrary[index].hasRead ? myLibrary[index].hasRead = false : myLibrary[index].hasRead = true
       readElement = document.querySelectorAll('.card')[index].children[3]
       readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`
     })
@@ -47,7 +43,7 @@ function createBookCard(bookList, book){
   card.classList.add('card')
   card.appendChild(document.createElement('h4')).innerHTML = `${book.title}`;
   card.appendChild(document.createElement('p')).innerHTML = `Author: ${book.author}`;
-  card.appendChild(document.createElement('p')).innerHTML = `Number of pages: ${book.pages}`;
+  card.appendChild(document.createElement('p')).innerHTML = `Pages: ${book.pages}`;
   card.appendChild(document.createElement('p')).innerHTML = `Has read? ${book.hasRead}`;
   return card
 }
@@ -56,6 +52,7 @@ function createButton(container, btnName) {
   dltBtn = container.appendChild(document.createElement('button'))
   dltBtn.innerHTML = btnName
   dltBtn.classList.add(btnName.toLowerCase())
+  dltBtn.classList.add('btn')
 }
 
 for (let i = 0; i < myLibrary.length; i++) {
@@ -63,10 +60,16 @@ for (let i = 0; i < myLibrary.length; i++) {
   dltBtn = bookCard.appendChild(document.createElement('button'))
   dltBtn.innerHTML = "Delete" 
   dltBtn.classList.add('delete')
+  dltBtn.classList.add('btn')
   readBtn = bookCard.appendChild(document.createElement('button'))
   readBtn.innerHTML = "Read" 
   readBtn.classList.add('read')
+  readBtn.classList.add('btn')
 }
+
+readBtns = document.querySelectorAll('.read')
+
+addClickEventForEdit(readBtns)
 
 addBook.addEventListener('click', () => {
   title = document.getElementById('title').value
@@ -79,14 +82,16 @@ addBook.addEventListener('click', () => {
   removeBtns = document.querySelectorAll('.delete')
   addClickEventForDelete(removeBtns)
   createButton(bookCard, "Read")
-  editBtns = document.querySelectorAll('.read')
-  addClickEventForEdit(editBtns)
+  readBtns = document.querySelectorAll('.read')
+  let index = readBtns.length - 1
+  readBtns[readBtns.length - 1].addEventListener('click', () =>{
+    myLibrary[index].hasRead ? myLibrary[index].hasRead = false : myLibrary[index].hasRead = true
+    readElement = document.querySelectorAll('.card')[index].children[3]
+    readElement.innerHTML = `Has read? ${myLibrary[index].hasRead}`
+  })
 })
 
 removeBtns = document.querySelectorAll('.delete')
 
 addClickEventForDelete(removeBtns)
 
-editBtns = document.querySelectorAll('.read')
-
-addClickEventForEdit(editBtns)
